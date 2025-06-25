ac0122ffe5b764be127652ccbbe79f798ceca68a
Magnus Orskov <magnusorskov@hotmail.com>
2025-06-25 21:00:16 +0200

    09 assignment

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
+    if (!Number.isInteger(numberOne) || !Number.isInteger(numberTwo)) {
+        return "ERROR";
+    } else if (numberOne < 0 || numberTwo < 0) {
+        return "ERROR";
+    }
 
+    if (numberOne > numberTwo) {
+        const temp = numberOne;
+        numberOne = numberTwo;
+        numberTwo = temp;
+    }
+    let sum = 0;
+    for (let i = numberOne; i <= numberTwo; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
