5313985cb11c9a3543d16b353141c030f165d9e7
Magnus Orskov <magnusorskov@hotmail.com>
2025-06-25 13:42:53 +0200

    repeat-string assignment

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, int) {
+    if (int < 0) {
+        return "ERROR";
+    }
+    let tempString = "";
+    for (let i = 0; i < int; i++) {
+        tempString = tempString + str;
+    }
+return tempString;
 };
 
 // Do not edit below this line
