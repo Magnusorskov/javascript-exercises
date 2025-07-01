fc4d71456daa7a8ff9b5702d8aaf6c381a029232
Magnus Orskov <magnusorskov@hotmail.com>
2025-07-01 21:10:09 +0200

    13_palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const wordWithoutSpaces = word.toLowerCase()
+        .split("")
+        .filter((character) =>
+            alphanumerical.includes(character))
+        .join("");
 
+    let leftIndex = 0;
+    let rightIndex = wordWithoutSpaces.length - 1;
+    let isPalindrome = true;
+    while (leftIndex < rightIndex && isPalindrome) {
+        if (wordWithoutSpaces.charAt(leftIndex) !== wordWithoutSpaces.charAt(rightIndex)) {
+            isPalindrome = false;
+        } else {
+            leftIndex++;
+            rightIndex--;
+        }
+    }
+    return isPalindrome;
 };
 
 // Do not edit below this line
