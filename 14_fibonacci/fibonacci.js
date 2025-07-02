3cf87a25277e02313c1ca184df9706c73a5ce79d
Magnus Orskov <magnusorskov@hotmail.com>
2025-07-02 11:36:28 +0200

    14_fibonacci, 15_getTheTitles, 16:findTheOldest

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
 
+    let count
+    if (typeof number !== 'number') {
+        count = parseInt(number)
+    } else {
+        count = number
+    }
+    if (count === 0) return 0;
+    if (count < 0) return "OOPS";
+
+
+    let firstNumber = 1;
+    let secondNumber = 0;
+    for (let i = 2; i <= number; i++) {
+        let current = firstNumber + secondNumber;
+        secondNumber = firstNumber;
+        firstNumber = current;
+    }
+    return firstNumber;
 };
 
 // Do not edit below this line
